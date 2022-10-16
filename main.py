74f03bf11fac5fdf03d23fea28646637ccd89562
atavice <atayigitvice@gmail.com>
2022-10-17 00:40:24 +0200

    ADD: Added Main.py
    Ata Viçe

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+baseurl = 'https://www.linkedin.com/jobs/search?keywords=working%2Bstudent&location=Munich&trk=guest_homepage-basic_jobs-search-bar_search-submit&currentJobId=3313824749&position=1&pageNum=0'
+
+mydriver = webdriver.Chrome('/chromedriver')
+mydriver.get(baseurl)
+time.sleep(5)
+mydriver.quit()
