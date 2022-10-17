bffc6f2c0c68ea4aaecbce014d7260aa21186a11
UfukYarisan <yarisan17@itu.edu.tr>
2022-10-17 17:32:39 +0200

    FIX: Changed Global Variable and Conventional Fixes
    Ufuk Yarışan

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-baseurl = 'https://www.linkedin.com/jobs/search?keywords=working%2Bstudent&location=Munich&trk=guest_homepage-basic_jobs-search-bar_search-submit&currentJobId=3313824749&position=1&pageNum=0'
+DRIVER_PATH = "./chromedriver"
+BASE_URL = "https://www.linkedin.com"
+EXTENTION_URL = "/jobs/search?keywords=working%2Bstudent&location=Munich&trk=guest_homepage-basic_jobs-search-bar_search-submit&currentJobId=3313824749&position=1&pageNum=0"
 
-mydriver = webdriver.Chrome('./chromedriver')
-mydriver.get(baseurl)
+mydriver = webdriver.Chrome(DRIVER_PATH)
+mydriver.get(BASE_URL + EXTENTION_URL)
 time.sleep(5)
 mydriver.quit()
