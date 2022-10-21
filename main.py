be6b5ed9c794cd3b3f9ec71146068d1dad8706b9
UfukYarisan <yarisan17@itu.edu.tr>
2022-10-21 16:17:47 +0200

    FIX: Changed Main & Actions Yaml to Fix Driver
    Ufuk Yarışan

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import os
 import time
-import csv
+import platform
+import chromedriver_autoinstaller
+
 import bs4 as bs
 import pandas as pd
-from datetime import date, datetime
+from datetime import date
+
 from math import ceil
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import platform
-import os
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By, Keys, Options, Service
+
 
 DRIVER_PATH = "./chromedriver"
 BASE_URL = "https://www.linkedin.com"
 EXTENTION_URL = "/jobs/search?keywords=Working%20Student&location=M%C3%BCnih%2C%20Bavyera%2C%20Almanya&locationId=&geoId=100477049&f_TPR=r86400&distance=25&position=1&pageNum=0"
 NUM_OF_JOBS_PER_REFRESH = 25
 
-mydriver = webdriver.Chrome(DRIVER_PATH)
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()
+chrome_options = webdriver.ChromeOptions()    
+ 
+options = ["--window-size=1200,1200", "--ignore-certificate-errors"]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+mydriver = webdriver.Chrome(options = chrome_options)
+# mydriver = webdriver.Chrome(DRIVER_PATH)
+
+
+
 mydriver.get(BASE_URL + EXTENTION_URL)
 
 HTML_code = mydriver.page_source
@@ -57,15 +76,14 @@ for link in soup.find_all('a', {'class': 'base-card__full-link absolute top-0 ri
     links.append(link.get('href'))
 
 today = date.today()
-now = datetime.now()
-current_time = now.strftime("%H-%M-%S")
 
 if platform.system == 'Darwin':
     os.makedirs('./job_links', exist_ok=True)
 
 df = pd.DataFrame(links)
-df.to_csv(f'./job_links/{today}_{current_time}.csv', header=None, index=False)
+df.to_csv(f'./job_links/{today}.csv', header=None, index=False)
 
 mydriver.quit()
+display.stop()
 
 
