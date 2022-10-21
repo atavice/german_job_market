8cf59a067678f1e6f73a59e2e54e39a1531be3d0
UfukYarisan <yarisan17@itu.edu.tr>
2022-10-21 16:54:54 +0200

    FIX: Fixed Imports
    Ufuk Yarışan

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ from datetime import date
 from math import ceil
 from selenium import webdriver
 from pyvirtualdisplay import Display
-from selenium.webdriver.common.by import By, Keys, Options, Service
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 DRIVER_PATH = "./chromedriver"
 BASE_URL = "https://www.linkedin.com"
